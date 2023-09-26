e96a9b1b3c0886b71757fefd561ff0a0c5a16992
Ankit Kushwaha <145086869+Ankit-K0@users.noreply.github.com>
2023-09-26 16:35:18 +0530

    Add files via upload

---
+++ Resources/index.js
@@ -0,0 +1,8 @@
+var menuicon = document.querySelector(".menu-icon")
+var sidebar = document.querySelector(".sidebar")
+var mainbox = document.querySelector(".main-box")
+
+menuicon.onclick = function(){
+    sidebar.classList.toggle("hidden-sidebar")
+    mainbox.classList.toggle("large-main-box")
+}
